06696a09827ef166b538e028759b67c18afd046d
englishwithmsnguyet-dev <englishwithmissnguyet@gmail.com>
2026-07-23 22:42:07 +0700

    Add ONB103 to Dạng 04 and 05

--- app.js
+++ app.js
@@ -410,9 +410,12 @@ class VstepApp {
                     return ['ONB103', 'CB206', 'CB210', 'CB211', 'CB213', 'missnguyet2026'];
                 }
                 if (id === 'p1_type_04') {
-                    return ['CB206', 'CB210', 'CB211', 'CB213'];
+                    return ['ONB103', 'CB206', 'CB210', 'CB211', 'CB213'];
                 }
-                if (id === 'p1_type_05' || id === 'p1_type_06') {
+                if (id === 'p1_type_05') {
+                    return ['ONB103', 'CB206', 'CB210', 'CB211'];
+                }
+                if (id === 'p1_type_06') {
                     return ['CB206', 'CB210', 'CB211'];
                 }
             } else {
