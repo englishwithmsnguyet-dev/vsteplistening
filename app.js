e8278b44b74d24dcf187e28e31d67d569fc0432a
englishwithmsnguyet-dev <englishwithmissnguyet@gmail.com>
2026-09-08 22:33:15 +0700

    Update B212 access to unlock all of Part 2

--- app.js
+++ app.js
@@ -454,13 +454,7 @@ class VstepApp {
 
         if (code === 'B212') {
             if (partNum === 1) return true;
-            if (partNum === 2) {
-                if (!id) return true; // Mở khóa giao diện Part 2
-                if (!isTheory && id.startsWith('p2_practice_')) {
-                    const pNum = parseInt(id.replace('p2_practice_', ''));
-                    if (pNum <= 4) return true;
-                }
-            }
+            if (partNum === 2) return true; // B212 mở hết PART 02
             return false;
         }
 
